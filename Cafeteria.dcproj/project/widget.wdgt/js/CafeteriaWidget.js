4178648fc889cb12321f095c7fdb7aa2de501ff5
Daniel Zoller <nosebrain@gmx.net>
2014-08-31 22:25:02 +0200

    added Cache for uni infos

--- Cafeteria.dcproj/project/widget.wdgt/js/CafeteriaWidget.js
+++ Cafeteria.dcproj/project/widget.wdgt/js/CafeteriaWidget.js
@@ -1,41 +1,52 @@
+var PREF_INFO_KEY = "uniInfos";
+
 var SERVICE_URL =  "http://localhost:9090/service";
-var SERVICE_AUTH = 'Basic Y2FmZXRlcmlhLXdpZGdldDpmaXJlZmx5';
-var CAFETERIA_INFOS;
 
 function CafeteriaWidget() {
-    $.ajax({
-        url: SERVICE_URL,
-        cache: false,
-        success: function(data) {
-            CAFETERIA_INFOS = data;
-            prepareSettings();
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            alert(xhr.status);
-            alert(thrownError);
-        }
-    });
+    this.prefs = new Pref();
+    var savedInfos = this.prefs.getPref(PREF_INFO_KEY, true);
+    var self = this;
+    if (savedInfos === undefined) {
+        alert("loading informations");
+        $.ajax({
+            url: SERVICE_URL,
+            success: function(data) {
+                self.gotInformation(data);
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert(xhr.status);
+                alert(thrownError);
+            }
+        });
+    } else {
+        this.infos = $(savedInfos);
+    }
 }
 
-function prepareSettings() {
+CafeteriaWidget.prototype.gotInformation = function(infos) {
+    this.prefs.savePref(PREF_INFO_KEY, infos, true);
+    this.infos = infos;
+    this.prepareSettings();
+}
+
+CafeteriaWidget.prototype.prepareSettings = function() {
     var uniPopup = $('#universityPopup');
     uniPopup.append($('<option></option>').attr('value', 'not_set').text("Bitte wählen ..."));
-    for (unikey in CAFETERIA_INFOS) {
-        var uni = CAFETERIA_INFOS[unikey];
+    for (unikey in this.infos) {
+        var uni = this.infos[unikey];
         uniPopup.append($('<option></option>').attr('value', unikey).text(uni.name));
     }
-    
+    var self = this;
     uniPopup.change(function() {
         var selectedValue = $(this).find('option:selected').attr('value');
         var cafeteriaPopup = $('#cafeteriaPopup');
         if (selectedValue != 'not_set') { // TODO: constant
             cafeteriaPopup.removeAttr('disabled').empty();
-            $(CAFETERIA_INFOS[selectedValue].cafeterias).each(function(index, cafeteria) {
+            $(self.infos[selectedValue].cafeterias).each(function(index, cafeteria) {
                 cafeteriaPopup.append($('<option></option>').attr('value', index).text(cafeteria.name));
             });
         } else {
             cafeteriaPopup.empty().attr('disabled', '');
         }
-        alert(selectedValue);
     });
 }
